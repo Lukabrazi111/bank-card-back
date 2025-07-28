5f072d1be7a64f6e38acbf8c2e23862a3aadbfd6
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-07-28 18:46:51 +0200

    feat: initial commit & add card model

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    public function index() {
+        // return only saved cards
+    }
+
+    public function store() {
+        // save card in database using custom form request
+    }
+}
